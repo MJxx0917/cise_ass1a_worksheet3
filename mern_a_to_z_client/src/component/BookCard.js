acbcc5c706eb62ced2078014d46109a8704b11a9
Michael Qu <MichaelQu@Meganss-MacBook-Air.local>
2022-08-03 10:54:18 +1200

    initial commit

---
+++ mern_a_to_z_client/src/component/BookCard.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../App.css';
+
+const BookCard = (props) => {
+    const  book  = props.book;
+
+    return(
+        <div className="card-container">
+            <img src="https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80" alt="" />
+            <div className="desc">
+                <h2>
+                    <Link to={`/show-book/${book._id}`}>
+                        { book.title }
+                    </Link>
+                </h2>
+                <h3>{book.author}</h3>
+                <p>{book.description}</p>
+            </div>
+        </div>
+    )
+};
+
+export default BookCard;
